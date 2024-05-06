d41daa765060bbaec3836723995e50bbe01938d3
gloven <foylin17364@gmail.com>
2024-05-06 13:11:40 +0300

    BottomNavigationView

--- app/src/main/java/com/example/financialapp/MainActivity.kt
+++ app/src/main/java/com/example/financialapp/MainActivity.kt
@@ -1,20 +1,47 @@
 package com.example.financialapp
 
+import android.app.FragmentTransaction
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.financialapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        bottomNavigationView.background = null
+        bottomNavigationView.menu.getItem(2).isEnabled = false
+
+        replaceFragment(HomeFragment())
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.statistics -> replaceFragment(StatsFragment())
+                R.id.settings -> replaceFragment(SettingsFragment())
+                R.id.info -> replaceFragment(InfoFragment())
+            }
+            true
         }
+    }
+
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager: FragmentManager = getSupportFragmentManager();
+        val fragmentTransaction: androidx.fragment.app.FragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
